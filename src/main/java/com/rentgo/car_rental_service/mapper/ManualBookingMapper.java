cb8569a67820365d38dcc38caaafbc1e4f2f5920
amalzaben <amaal.theeb66@gmail.com>
2026-01-25 21:10:17 +0200

    mapper annotation

--- src/main/java/com/rentgo/car_rental_service/mapper/ManualBookingMapper.java
+++ src/main/java/com/rentgo/car_rental_service/mapper/ManualBookingMapper.java
@@ -2,7 +2,9 @@ package com.rentgo.car_rental_service.mapper;
 
 import com.rentgo.car_rental_service.dto.controller.request.ManualCreateBookingRequest;
 import com.rentgo.car_rental_service.dto.service.ManualCreateBookingCommand;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface ManualBookingMapper {
     ManualCreateBookingCommand toCommand(Long employeeId, ManualCreateBookingRequest request);
 }
